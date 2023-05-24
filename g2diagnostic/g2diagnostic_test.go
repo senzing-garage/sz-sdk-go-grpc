34b6172fd4968b47582158f57c5e5a1966ac58ff
docktermj <michael@dockter.com>
2023-05-24 08:53:58 -0400

    Migrate from Error() to NewError()

--- g2diagnostic/g2diagnostic_test.go
+++ g2diagnostic/g2diagnostic_test.go
@@ -47,7 +47,7 @@ var (
 // ----------------------------------------------------------------------------
 
 func createError(errorId int, err error) error {
-	return g2error.Cast(localLogger.Error(errorId, err), err)
+	return g2error.Cast(localLogger.NewError(errorId, err), err)
 }
 
 func getGrpcConnection() *grpc.ClientConn {
