27b31020abc4ff82747f8151afb4fa7f01c37134
docktermj <michael@dockter.com>
2024-08-23 15:25:20 -0400

    #180 Add documentation

--- szconfig/main.go
+++ szconfig/main.go
@@ -8,4 +8,4 @@ package szconfig
 ComponentID is the identifier of the szconfig package.
 szconfig package messages will have the format "SZSDK6021eeee" where "eeee" is the error identifier.
 */
-*/const ComponentID = 6021
+const ComponentID = 6021
